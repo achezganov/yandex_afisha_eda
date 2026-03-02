d2a0fc7c8cf4fd7bd503cc8e7e97ba35fdcb9b1b
Alexey <i@achezganov.ru>
2026-03-02 13:42:02 +0500

    feat(preprocessing): добавить предобработку данных
    Расширен пайплайн подготовки датасета после базовой валидации:
    
    * приведены order_dt и order_ts к корректным типам;
    * добавлены временные признаки (год, месяц, день, weekday, час);
    * реализована конвертация выручки KZT -> RUB с созданием revenue_rub;
    * удалены выбросы по 99-му процентилю (tickets_count, days_since_prev, revenue_rub);
    * оптимизированы типы числовых признаков для снижения потребления памяти;
    * нормализованы текстовые поля (service_name, region_name, city_name);
    * выявлены и удалены неявные дубликаты заказов по бизнес-ключу.

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def _(pd):
     # Выполняем SQL-запрос и загружаем результат в pandas DataFrame,
     # затем сохраняем результат в CSV файл
     # datasets/dataset.csv — файл, который будет использоваться далее для анализа
-    pd.read_sql(query, conn).to_csv('datasets/dataset.csv')
+    pd.read_sql(query, conn).to_csv('datasets/dataset.csv', index=False)
 
 
     # Позже этот CSV можно загрузить обратно в DataFrame для анализа:
@@ -152,20 +152,20 @@ def _(mo):
 @app.cell
 def _(pd):
     df_original = pd.read_csv('datasets/dataset.csv')
-    df = df_original.copy()
-    return (df,)
+    df_raw = df_original.copy()
+    return (df_raw,)
 
 
 @app.cell
-def _(df):
-    df.info()
+def _(df_raw):
+    df_raw.info()
     return
 
 
 @app.cell
-def _(df):
+def _(df_raw):
     # Выведем данные с начала и с конца датафрейма
-    df.head(), df.tail()
+    df_raw.head(), df_raw.tail()
     return
 
 
@@ -264,14 +264,14 @@ def _(pd):
 @app.cell
 def _():
     # Проверим датасет на валидность 1
-    #validate_dataset(df)
+    #validate_dataset(df_raw)
     return
 
 
 @app.cell
-def _(df):
+def _(df_raw):
     # Обработка отрицательного revenue 
-    f'Доля отрицательных значений в поле revenue: {round(df[df['revenue'] < 0].shape[0] * 100 / df.shape[0], 2)}%'
+    f'Доля отрицательных значений в поле revenue: {round(df_raw[df_raw['revenue'] < 0].shape[0] * 100 / df_raw.shape[0], 2)}%'
     return
 
 
@@ -284,20 +284,20 @@ def _(mo):
 
 
 @app.cell
-def _(df):
+def _(df_raw):
     #Находим индексы строк с отрицательным revenue
-    bad_idx = df.index[df["revenue"] < 0]
+    bad_idx = df_raw.index[df_raw["revenue"] < 0]
 
     # Удаляем их из df + сбрасываем индекс
-    df.drop(index=bad_idx, inplace=True)
-    df.reset_index(drop=True, inplace=True)
+    df_raw.drop(index=bad_idx, inplace=True)
+    df_raw.reset_index(drop=True, inplace=True)
     return
 
 
 @app.cell
-def _(df, validate_dataset):
+def _(df_raw, validate_dataset):
     # Проверим датасет на валидность 2
-    validate_dataset(df)
+    validate_dataset(df_raw)
     return
 
 
@@ -307,7 +307,7 @@ def _(mo):
     **Промежуточный вывод:**
 
     1. **Объём и структура данных.**
-    Мы успешно загрузили датасет, который содержит 290 849 записей и 15 колонок (~33.3 mb). Структура данных соответствует ожидаемой, все необходимые признаки присутствуют. Типы данных корректны и позволяют проводить дальнейший анализ.
+    Мы успешно загрузили датасет, который содержит 290 849 записей и 14 колонок (~31 mb). Структура данных соответствует ожидаемой, все необходимые признаки присутствуют. Типы данных корректны и позволяют проводить дальнейший анализ.
 
     2. **Качество данных.**
     В ходе первичного анализа мы обнаружили пропуски только в поле `days_since_prev`. Это ожидаемо, поскольку для пользователей с одной покупкой отсутствует предыдущая транзакция. Некорректных значений в полях `order_dt` и `order_ts` не выявлено — данные успешно приводятся к форматам даты и времени. Остальные ключевые признаки содержат допустимые значения.
@@ -319,6 +319,325 @@ def _(mo):
     В процессе проверки мы убедились, что дубликаты по ключевому полю `order_id` отсутствуют. Также не выявлено критических ошибок, которые могли бы повлиять на результаты анализа. Все признаки находятся в корректном формате.
 
     На этапе предобработки можно расширить признаковое пространство за счет декомпозиции временных признаков. Это позволит анализировать сезонность, активность по дням недели и по времени суток. А также можно оптимизировать типы данных для уменьшения объема памяти.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## 5. Предобработка данных
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.1 Обрабтка категориальных признаков
+    """)
+    return
+
+
+@app.cell
+def _(df_raw):
+    # Проверим event_type_main на некорректные значения
+    df_raw['event_type_main'].unique()
+    return
+
+
+@app.cell
+def _(df_raw):
+    # Проверим currency_code на некорректные значения
+    df_raw['currency_code'].unique()
+    return
+
+
+@app.cell
+def _(df_raw):
+    # Проверим service_name на некорректные значения
+    df_raw['service_name'].unique()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.2 Обработка временных признаков и расширение признакового пространства
+    """)
+    return
+
+
+@app.cell
+def _(df_raw, pd):
+    # Приводим тип полей order_dt и order_ts к date и datetime 
+    df_raw['order_dt'] = pd.to_datetime(df_raw['order_dt'], errors='coerce').dt.date
+    df_raw['order_ts'] = pd.to_datetime(df_raw['order_ts'], errors='coerce')
+    return
+
+
+@app.cell
+def _(df_raw):
+    # расширим признаковое пространство, за счет декомпозиции временных признаков
+    df_raw['order_year'] = df_raw['order_ts'].dt.year.astype('int16')
+    df_raw['order_month'] = df_raw['order_ts'].dt.month.astype('int8')
+    df_raw['order_day'] = df_raw['order_ts'].dt.day.astype('int8')
+    df_raw['order_weekday'] = df_raw['order_ts'].dt.weekday.astype('int8')
+    df_raw['order_hour'] = df_raw['order_ts'].dt.hour.astype('int8')
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.3 Приведение выручки к единой валюте (2.1)
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    # Загружаем датасет с курсом тенге
+    tenge_rate_df = pd.read_csv('datasets/final_tickets_tenge_df.csv'
+    )
+
+    # Посмотрим структуру
+    tenge_rate_df.head(), tenge_rate_df['cdx'].unique(), tenge_rate_df['nominal'].unique()
+    return (tenge_rate_df,)
+
+
+@app.cell
+def _(df_raw, pd, tenge_rate_df):
+    # Рассчитываем курс: сколько рублей за 1 тенге
+    tenge_rate_df['kzt_to_rub'] = tenge_rate_df['curs'] / tenge_rate_df['nominal']
+
+    # Приводим дату к формату date для корректного merge
+    tenge_rate_df['order_dt'] = pd.to_datetime(tenge_rate_df['data']).dt.date
+
+    # Добавляем курс в основной датафрейм
+    df = df_raw.merge(tenge_rate_df[['order_dt', 'kzt_to_rub']], 
+                              on='order_dt', 
+                              how='left')
+    return (df,)
+
+
+@app.cell
+def _(df):
+    # Создаем новый столбец revenue_rub
+    # если валюта kzt — конвертируем через курс, иначе не меняем
+    df['revenue_rub'] = df['revenue']
+
+    mask_kzt = df['currency_code'] == 'kzt'
+    df.loc[mask_kzt, 'revenue_rub'] = (
+        df.loc[mask_kzt, 'revenue'] * df.loc[mask_kzt, 'kzt_to_rub']
+    )
+
+    # Удаляем вспомогательный столбец курса
+    df.drop(columns=['kzt_to_rub'], inplace=True)
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим результат 
+    df[df['currency_code'] =='kzt'].head(), df[df['currency_code'] =='rub'].head()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.4 Обработка числовых признаков
+    """)
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим статистические показатели полей, что понять, можно ли их сжать
+    fields = ['tickets_count', 'days_since_prev', 'event_id', 'order_id', 'revenue_rub']
+    df[fields].describe()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    В полях `tickets_count`, `days_since_prev` и `revenue_rub` обнаружены критические выбросы. Мы избавимся от них, используя 99-й процентиль.
+    """)
+    return
+
+
+@app.cell
+def _(df):
+    # На основе стат. показателей выберем подходящие типы для полей
+    df['days_since_prev'] = df['days_since_prev'].astype('float32')
+    df['tickets_count'] = df['tickets_count'].astype('uint8')
+    df[['event_id', 'order_id']] = df[['event_id', 'order_id']].astype('int32')
+
+    # Приведение финансовых показателей
+    df[['revenue', 'revenue_rub']] = df[['revenue', 'revenue_rub']].astype('float32')
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Отметим, что поля `revenue` и `revenue_rub` не следует сжимать, так как они являются финансовыми показателями, и потеря точности может быть критичной для корректности денежных агрегатов. Однако для аналитических расчётов допустимо приведение к типу `float32`.
+    """)
+    return
+
+
+@app.cell
+def _(df):
+    # Избавляемся от выбросов по 99 процентилю
+    outliers_idx = df.index[(df["tickets_count"] > df['tickets_count'].quantile(0.99))
+                          | (df["days_since_prev"] > df['days_since_prev'].quantile(0.99))
+                          | (df["revenue_rub"] > df['revenue_rub'].quantile(0.99))]
+
+    print(f'Суммарная доля выбросов в полях day_since_prev, revenue_rub и tickets_count: {round(len(outliers_idx) * 100 / df.shape[0], 2)}%')
+    return (outliers_idx,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Суммарная доля выбросов и, ранее удаленных, аномальных зачений не превышает 5%. Можем избавиться от выбросов.
+    """)
+    return
+
+
+@app.cell
+def _(df, outliers_idx):
+    # Удаляем их из df + сбрасываем индекс
+    df.drop(index=outliers_idx, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    print(f"Размер df после удаления выбросов: {df.shape}")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.5 Нормализация, обработка явных и неявных дубликатов
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(pd):
+    # Функция нормализации текстовых полей
+    def normalize_text_column(series: pd.Series) -> pd.Series:
+        """
+        Нормализует строковое поле:
+        - приводит к строковому типу
+        - убирает лишние пробелы
+        - приводит к нижнему регистру
+        - заменяет множественные пробелы на один
+        - убирает пробелы в начале и конце
+        """
+        return (
+            series
+            .astype("string")
+            .str.strip()
+            .str.lower()
+            .str.replace(r"\s+", " ", regex=True)
+        )
+
+    return (normalize_text_column,)
+
+
+@app.cell
+def _(df, normalize_text_column):
+    # Применяем нормализацию к нужным полям
+    columns_to_normalize = ['service_name', 'region_name', 'city_name']
+
+    for col in columns_to_normalize:
+        df[col] = normalize_text_column(df[col])
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим на явные дубликаты
+    explicit_duplicates = df.duplicated()
+
+    print(f"Количество явных дубликатов: {explicit_duplicates.sum()}")
+    print(f"Доля явных дубликатов: {round(explicit_duplicates.mean() * 100, 2)}%")
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим на неявные дубликаты по ключевому бизнес-идентификатору order_id
+    implicit_duplicates_order = df.duplicated(subset=['order_id'])
+
+    print(f"Количество неявных дубликатов по order_id: {implicit_duplicates_order.sum()}")
+    print(f"Доля неявных дубликатов по order_id: {round(implicit_duplicates_order.mean() * 100, 2)}%")
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим на неявные дубликаты заказов одного пользователя в один момент времени
+    implicit_duplicates_user_time = df.duplicated(
+        subset=['user_id', 'order_ts', 'event_id', 'service_name']
+    )
+
+    print(f"Количество потенциальных дублей заказов: {implicit_duplicates_user_time.sum()}")
+    print(f"Доля потенциальных дублей заказов: {round(implicit_duplicates_user_time.mean() * 100, 3)}%")
+
+    df[implicit_duplicates_user_time].head()
+    return
+
+
+@app.cell
+def _(df):
+    # Удаляем найденные неявные дубликаты
+    df.drop_duplicates(
+        subset=['user_id', 'order_ts', 'event_id', 'service_name'],
+        keep='first',  # оставляем первый (предположительно оригинальный) заказ
+        inplace=True
+    )
+
+    df.reset_index(drop=True, inplace=True)
+    return
+
+
+@app.cell
+def _(df):
+    # Проверим, что дубликаты удалены
+    implicit_duplicates_user_time_after = df.duplicated(
+        subset=['user_id', 'order_ts', 'event_id', 'service_name']
+    )
+
+    print(f"Количество дублей после удаления: {implicit_duplicates_user_time_after.sum()}")
+    print(f"Размер df после удаления дублей: {df.shape}")
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### 5.6 Промежуточный вывод по предобработке
+    В ходе предобработки данных мы последовательно привели датасет к состоянию, пригодному для дальнейшего исследовательского анализа и построения моделей.
+
+    Мы обработали категориальные признаки: унифицировали текстовые значения, устранили различия в регистре, лишние пробелы и неоднородности написания.
+
+    Расширили временные признаки на основе исходного timestamp заказа. Были выделены год, месяц, день, день недели и час заказа. Это позволило подготовить данные для анализа сезонности, поведенческих паттернов пользователей и временных закономерностей спроса.
+
+    Также мы привели выручку к единой валюте и сформировали показатель `revenue_rub`, что устранило проблему несопоставимости денежных значений и обеспечило корректность дальнейших расчетов метрик выручки.
+
+    В числовых признаках мы провели анализ выбросов. Суммарная доля выбросов в полях `days_since_prev`, `revenue_rub` и `tickets_count` составила 1.94%. Это небольшая доля наблюдений, поэтому их удаление не оказывает существенного влияния на репрезентативность выборки, но при этом позволяет снизить влияние аномальных значений на статистики. После удаления выбросов размер датасета составил 284 846 наблюдений.
+
+    Выполнили нормализацию текстовых признаков, приведя их к единому формату.
+
+    Проверка на явные дубликаты показала их отсутствие (0 наблюдений). Также мы проверили дубликаты по бизнес-ключу `order_id` и убедились, что каждая запись имеет уникальный идентификатор заказа.
+
+    Дополнительно мы выявили потенциальные неявные дубликаты — случаи, когда у одного пользователя зафиксирован заказ с одинаковыми `order_ts`, `event_id` и `service_name`. Было обнаружено 117 таких записей (0.041% выборки). С высокой вероятностью это технические дубликаты, поэтому мы удалили их, сохранив первое вхождение. После этого финальный размер датасета составил 284 729 наблюдений, и повторная проверка подтвердила отсутствие дубликатов.
+
+    В результате мы получили чистый датасет, в котором устранены основные проблемы качества данных, способные исказить результаты исследования.
     """)
     return
 
